488955ace6f955882203708f9cb290a966fb27a9
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-06-06 15:26:35 -0500

    setup hard coded mock api for Users

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,22 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
+app.get("/users", function (req, res) {
+  {
+    // Hard Codeing
+    res.json([
+      { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+      { id: 2, firstName: "John", lastName: "Doe", email: "john@gmail.com" },
+      {
+        id: 3,
+        firstName: "Mark",
+        lastName: "Hazleton",
+        email: "mark@gmail.com",
+      },
+    ]);
+  }
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
