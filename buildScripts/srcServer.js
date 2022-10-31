36489c84c440e5f29c9f97282a1b0a855fd1cde4
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-10-30 20:14:10 -0500

    update packages

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -24,8 +24,18 @@ app.get("/users", function (req, res) {
   {
     // Hard Codeing
     res.json([
-      { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
-      { id: 2, firstName: "John", lastName: "Doe", email: "john@gmail.com" },
+      {
+        id: 1,
+        firstName: "Bob",
+        lastName: "Smith",
+        email: "bob@gmail.com",
+      },
+      {
+        id: 2,
+        firstName: "John",
+        lastName: "Doe",
+        email: "john@gmail.com",
+      },
       {
         id: 3,
         firstName: "Mark",
